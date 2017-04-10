8d440f722c4791f19c75d539fbc8a564b55ab13f
David Menting <david@davidmenting.nl>
2017-04-10 21:46:15 +0200

    note_stack is now a separate header and cpp file

--- src/note_stack.h
+++ src/note_stack.h
@@ -41,6 +41,8 @@
 #ifndef SHRUTHI_NOTE_STACK_H_
 #define SHRUTHI_NOTE_STACK_H_
 
+#include <cstdint>
+
 static const uint8_t kNoteStackSize = 8;
 
 struct NoteEntry {
@@ -88,92 +90,6 @@ class NoteStack {
   NoteEntry pool_[kNoteStackSize + 1];  // First element is a dummy node!
   uint8_t root_ptr_;  // Base 1.
   uint8_t sorted_ptr_[kNoteStackSize + 1];  // Base 1.
-
 };
-
-static const uint8_t kFreeSlot = 0xff;
-
-void NoteStack::NoteOn(uint8_t note, uint8_t velocity) {
-  // Remove the note from the list first (in case it is already here).
-  NoteOff(note);
-  // In case of saturation, remove the least recently played note from the
-  // stack.
-  if (size_ == kNoteStackSize) {
-    uint8_t least_recent_note;
-    for (uint8_t i = 1; i <= kNoteStackSize; ++i) {
-      if (pool_[i].next_ptr == 0) {
-        least_recent_note = pool_[i].note;
-      }
-    }
-    NoteOff(least_recent_note);
-  }
-  // Now we are ready to insert the new note. Find a free slot to insert it.
-  uint8_t free_slot;
-  for (uint8_t i = 1; i <= kNoteStackSize; ++i) {
-    if (pool_[i].note == kFreeSlot) {
-      free_slot = i;
-    }
-  }
-  pool_[free_slot].next_ptr = root_ptr_;
-  pool_[free_slot].note = note;
-  pool_[free_slot].velocity = velocity;
-  root_ptr_ = free_slot;
-  // The last step consists in inserting the note in the sorted list.
-  for (uint8_t i = 0; i < size_; ++i) {
-    if (pool_[sorted_ptr_[i]].note > note) {
-      for (uint8_t j = size_; j > i; --j) {
-        sorted_ptr_[j] = sorted_ptr_[j - 1];
-      }
-      sorted_ptr_[i] = free_slot;
-      free_slot = 0;
-      break;
-    }
-  }
-  if (free_slot) {
-    sorted_ptr_[size_] = free_slot;
-  }
-  ++size_;
-}
-
-void NoteStack::NoteOff(uint8_t note) {
-  uint8_t current = root_ptr_;
-  uint8_t previous = 0;
-  while (current) {
-    if (pool_[current].note == note) {
-      break;
-    }
-    previous = current;
-    current = pool_[current].next_ptr;
-  }
-  if (current) {
-    if (previous) {
-      pool_[previous].next_ptr = pool_[current].next_ptr;
-    } else {
-      root_ptr_ = pool_[current].next_ptr;
-    }
-    for (uint8_t i = 0; i < size_; ++i) {
-      if (sorted_ptr_[i] == current) {
-        for (uint8_t j = i; j < size_ - 1; ++j) {
-          sorted_ptr_[j] = sorted_ptr_[j + 1];
-        }
-        break;
-      }
-    }
-    pool_[current].next_ptr = 0;
-    pool_[current].note = kFreeSlot;
-    pool_[current].velocity = 0;
-    --size_;
-  }
-}
-
-void NoteStack::Clear() {
-  size_ = 0;
-  memset(pool_ + 1, 0, sizeof(NoteEntry) * kNoteStackSize);
-  memset(sorted_ptr_ + 1, 0, kNoteStackSize);
-  root_ptr_ = 0;
-  for (uint8_t i = 0; i <= kNoteStackSize; ++i) {
-    pool_[i].note = kFreeSlot;
-  }
-}
 
 #endif // SHRUTHI_NOTE_STACK_H_
