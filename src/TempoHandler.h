413b8f8bd7f4167e13ad3bed0d979433b42b1269
David Menting <david@davidmenting.nl>
2022-12-22 16:47:45 +0100

    Go up to 600 BPM

--- src/TempoHandler.h
+++ src/TempoHandler.h
@@ -144,17 +144,18 @@ class TempoHandler
       _sync_pin_previous_value = _sync_pin_value;
     }
 
-    #define BPM_TO_MILLIS(bpm) (5000000 / bpm)
+    #define BPM_TO_MILLIS(bpm) (2500000 / bpm)
     void update_internal() {
       int potvalue = synth.speed;
       uint32_t scaled_millis_per_beat; // 2 x beats per minute
 
       if(potvalue < 128) {
-        scaled_millis_per_beat = map(potvalue,0,128,BPM_TO_MILLIS(60),BPM_TO_MILLIS(120));
+        scaled_millis_per_beat = map(potvalue,0,128,BPM_TO_MILLIS(30),BPM_TO_MILLIS(60));
       } else if(potvalue < 895) {
-        scaled_millis_per_beat = map(potvalue, 128,895,BPM_TO_MILLIS(120),BPM_TO_MILLIS(400));
+        scaled_millis_per_beat = map(potvalue, 128,895,BPM_TO_MILLIS(60),BPM_TO_MILLIS(200));
       } else {
-        scaled_millis_per_beat = map(potvalue, 895,1023,BPM_TO_MILLIS(400),BPM_TO_MILLIS(1200));
+        // For Toon: 603 BPM in gives 600 BPM out
+        scaled_millis_per_beat = map(potvalue, 895,1023,BPM_TO_MILLIS(200),BPM_TO_MILLIS(603));
       }
 
       const auto cur = millis();
